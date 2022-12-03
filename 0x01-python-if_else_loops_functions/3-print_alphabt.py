12892d854ffb706f57b4562451f4b63a60c51349
Henry kokobili <henrykokobili@gmail.com>
2022-12-03 14:06:25 -0800

    pytask

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for char in range(ord("a"), ord("z") + 1):
     if chr(char) == 'e' or chr(char) == 'q':
         continue
-    print("{:c}".format(char), end='')
+        print("{:c}".format(char), end='')
